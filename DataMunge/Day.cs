9e994160b51664dbddad3acea6d8f9842dd825e1
KENT\cbovein1 <cbovein1@kent.edu>
2018-09-27 09:26:15 -0400

    Initial commit - Part 1

---
+++ DataMunge/Day.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataMunge
+{
+    class Day
+    {
+
+        public string DayOfMonth { get; set; }
+        public string DayOfWeek { get; set; }
+        public double High { get; set; }
+        public double Low { get; set; }
+        public int ChanceOfPrecipitation { get; set; }
+
+        public Day(string dayOfMonth, string dayOfWeek, string high, string low, string chanceOfPrecipitation)
+        {
+            DayOfMonth = ConvertNumToOrdinal(dayOfMonth);
+            DayOfWeek = ConvertLetterToWord(dayOfMonth);
+            High = CelsiusToFarenheit(high);
+            Low = CelsiusToFarenheit(low);
+            ChanceOfPrecipitation = int.Parse(chanceOfPrecipitation);
+        }
+
+        private double CelsiusToFarenheit(string celsius)
+        {
+            return double.Parse(celsius) * 9.0 / 5.0 + 32.0;
+        }
+
+        private string ConvertNumToOrdinal(string dayOfMonth)
+        {            
+            string day = dayOfMonth;
+            if (dayOfMonth.EndsWith("11")) day = dayOfMonth + "th";
+            else if (dayOfMonth.EndsWith("12")) day = dayOfMonth + "th";
+            else if (dayOfMonth.EndsWith("11")) day = dayOfMonth + "th";
+            else if (dayOfMonth.EndsWith("1")) day = dayOfMonth + "st";
+            else if (dayOfMonth.EndsWith("2")) day = dayOfMonth + "nd";
+            else if (dayOfMonth.EndsWith("3")) day = dayOfMonth + "rd";
+            else day = dayOfMonth + "th";
+
+            return day;
+        }
+
+        private string ConvertLetterToWord(string dayOfMonth)
+        {
+            //TODO: Make this work for every month
+            string dayOfWeek = string.Empty;
+            switch(int.Parse(dayOfMonth) % 7)
+            {
+                case 1:
+                    dayOfWeek = "Wednesday";
+                    break;
+                case 2:
+                    dayOfWeek = "Thursday";
+                    break;
+                case 3:
+                    dayOfWeek = "Friday";
+                    break;
+                case 4:
+                    dayOfWeek = "Saturday";
+                    break;
+                case 5:
+                    dayOfWeek = "Sunday";
+                    break;
+                case 6:
+                    dayOfWeek = "Monday";
+                    break;
+                case 0:
+                    dayOfWeek = "Tuesday";
+                    break;
+            }
+            return dayOfWeek;
+        }
+
+        public void Print()
+        {
+            Console.Write(DayOfMonth + ", ");
+            Console.Write(DayOfWeek + ", ");
+            Console.Write(High + ", ");
+            Console.Write(Low + ", ");
+            Console.Write(ChanceOfPrecipitation + "\n");            
+        }
+    }
+}
---
+++ DataMunge/Main.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataMunge
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string line = string.Empty;
+            List<Day> days = new List<Day>();
+            StreamReader weatherFile = new StreamReader(@"C:\Users\cbovein1\Desktop\weather.txt");
+            string headerLine = weatherFile.ReadLine();
+            while ((line = weatherFile.ReadLine()) != null)
+            {
+                string[] spl = line.Split((string[])null, StringSplitOptions.RemoveEmptyEntries);
+                days.Add(new Day(spl[0], spl[1], spl[2], spl[3], spl[4]));
+            }
+
+            days = days.OrderBy(p => p.ChanceOfPrecipitation).Where(t => t.High <= 85 && t.Low >= 70).ToList();
+            //PrintAllDays(days);
+
+            PrintTheBestDay(days);
+        }
+
+        private static void PrintTheBestDay(List<Day> days)
+        {
+            Day day = days.FirstOrDefault();
+            Console.WriteLine(string.Format("{0} the {1} day of the month is the best day for a picnic.", day.DayOfWeek, day.DayOfMonth));
+        }
+
+        private static void PrintAllDays(List<Day> days)
+        {
+            foreach (var d in days)
+            {
+                d.Print();
+            }
+        }
+    }
+}
